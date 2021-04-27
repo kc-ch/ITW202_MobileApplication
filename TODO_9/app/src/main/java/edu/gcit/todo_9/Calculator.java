133092b7db375384ac823b84487e6ff7f6e838d8
unknown <karmachoden238@gmail.com>
2021-04-27 12:48:06 +0600

    commit

---
+++ TODO_9/app/src/main/java/edu/gcit/todo_9/Calculator.java
@@ -0,0 +1,16 @@
+package edu.gcit.todo_9;
+
+public class Calculator {
+    public double addition(double value1, double value2){
+        return value1 + value2;
+    }
+    public double subtraction(double value1, double value2){
+        return value1 - value2;
+    }
+    public double division(double value1, double value2){
+        return value1 / value2;
+    }
+    public double multiplication(double value1, double value2){
+        return value1 * value2;
+    }
+}
