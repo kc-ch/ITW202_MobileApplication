555c4510cd3087d7d3e5c610be58a32886032e65
unknown <karmachoden238@gmail.com>
2021-05-25 21:27:14 +0600

    commit

---
+++ TODO_21/app/src/main/java/com/gcit/todo_21/MainActivity.java
@@ -0,0 +1,23 @@
+package com.gcit.todo_21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView mTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.textView);
+    }
+
+    public void startTask(View view) {
+        mTextView.setText("Napping.....");
+        new SimpleAsyncTask(mTextView).execute();
+    }
+}
